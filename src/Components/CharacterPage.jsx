b68cf0760649cfddebd0f45554252ab547ab2cad
iamwales <adeogunolawale@gmail.com>
2022-02-09 14:07:27 +0100

    route update

--- src/Components/CharacterPage.jsx
+++ src/Components/CharacterPage.jsx
@@ -1,25 +1,27 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function CharacterPage({match,  char_id}) {
+function CharacterPage({ match, char_id }) {
+  console.log("I got here");
 
-    const[items, setItems]=useState([]);
-  
-    useEffect(() => {
-      const fetchItems = async () =>{
-        const { result } = await axios(
-          `https:www.breakingbadapi.com/api/characters/${match.params.char_id}`)
-  
-        setItems(result);
-      }
-  
-      fetchItems();
-    } )
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const { data } = await axios(
+        `https:www.breakingbadapi.com/api/characters/${match.params.char_id}`
+      );
+      console.log("result", data);
+      setItems(data[0]);
+    };
+
+    fetchItems();
+  }, [setItems, match]);
 
   return (
-        <div>
-            <h1>{items.name}</h1>
-        </div>
+    <div>
+      <h1>{items && items.name ? items.name : "Unable to get Item"}</h1>
+    </div>
   );
 }
 
