597fce1653177f7d8548bd990596fd913d4d0ff3
unknown <tamiloreolaniyi@gmail.com>
2022-03-03 20:09:13 +0100

    final commit

--- src/Components/CharacterPage.jsx
+++ src/Components/CharacterPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function CharacterPage({ match, char_id }) {
+function CharacterPage({ match, char_id, isLoading }) {
   console.log("I got here");
 
   const [items, setItems] = useState([]);
@@ -19,8 +19,17 @@ function CharacterPage({ match, char_id }) {
   }, [setItems, match]);
 
   return (
-    <div>
-      <h1>{items && items.name ? items.name : "Unable to get Item"}</h1>
+    <div className="character">
+      <div className="left">
+      <img src={items.img} alt="" />
+      </div>
+      <div className="right">
+        <h3>Name: {items.name}</h3>
+        <h3>Birthday: {items.birthday} </h3>
+        <h3>Status: {items.status} </h3>
+        <h3>Nickname: {items.nickname} </h3>
+        <h3>Portrayed: {items.portrayed} </h3>
+      </div>
     </div>
   );
 }
